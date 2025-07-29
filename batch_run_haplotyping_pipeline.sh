665577f4319d3ff2cfeb4aa5515ffb9544393d12
alienshe <sylvie.nolf@gmail.com>
2025-07-29 16:26:28 +1000

    renamed scripts

---
+++ batch_run_haplotyping_pipeline.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 6 ]; then
+    echo "Error: incorrect number of arguments"
+    echo "Usage: ./batch_run_devider_pipeline.sh <list.tsv> <input_dir> <output_dir> <query_dir> <reference_dir> <minimum_abundance>"
+    echo "note that input_dir and output_dir must be absolute paths (starting from home), and query_dir and reference_dir should be relative paths from input_dir"
+    echo "see inside script for more information on arguments"
+    exit 1
+fi
+INPUT_TSV="$1"
+#^ tab sep file with the following columns: name of input file in query dir, name of reference file in ref dir, and the name of the gene (just so downstream file names don't get too messy
+INPUT_DIR="$2"
+#^ Directory containing both the query and ref dirs, must be a non-relative path eg: /home/tobias-lab/sylvie_workspace
+OUTPUT_DIR="$3"
+#^output directory, must be a non-relative path eg: /home/tobias-lab/devider_outputs
+QUERY_DIR="$4"
+#^ relative path from INPUT_DIR. Contains all input fastq files eg pangkep_1/demultiplexed_fastqs
+REF_DIR="$5"
+#^ relative path from INPUT_DIR. Contains all the reference files eg reference_files
+MIN_AB="$6"
+#^ number from  0-100  minimum abundance for a haplotype - input to devider
+
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR" || { echo "Failed to cd into $OUTPUT_DIR"; exit 1; }
+
+
+
+SCRIPT_NAME=$0
+script_path=$(dirname "$0")
+while IFS=$'\t' read -r query reference gene_name || [ -n "$query" ]; do
+    query=$(echo "$query" | tr -d '\r')
+    reference=$(echo "$reference" | tr -d '\r')
+    gene_name=$(echo "$gene_name" | tr -d '\r')
+    echo "Processing:"
+    echo "Query: $query"
+    echo "Reference: $reference"
+    echo "Gene: $gene_name"
+
+    # Example command using query and reference
+    ${script_path}/haplotyping_pipeline.sh "$INPUT_DIR" "$QUERY_DIR/$query" "$REF_DIR/$reference" "$OUTPUT_DIR/$gene_name" "$MIN_AB" "$gene_name"
+done < "$INPUT_TSV"
+
+echo -e "copying alignments into one folder..."
+${script_path}/intermediate/alignment_collector.sh "$OUTPUT_DIR" "$OUTPUT_DIR"/collected_alignments
+
+
+echo -e "\n****** FINAL SUMMARY ******"
+${script_path}/intermediate/devider_output_summarizer.sh "$OUTPUT_DIR" "$OUTPUT_DIR/haplotype_summary.txt"
+echo -e "\nAlignments collected in $OUTPUT_DIR/collected_alignments (where devider ran, alignments are tagged with haplotype 'HP')"
---
+++ haplotyping_pipeline.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+#positional args:
+INPUT_DIR=$1 #directory containing reads and ref fasta, absolute path needed
+READS=$2 #relative to input directory, must be trimmed already eg with porechop
+REFERENCE_FASTA=$3 #relative to input directory, subset of the relevant chromosome, no special characters in header of fasta, must be indexed with samtools (.fasta.fai file in same directory)
+OUTPUT_DIR=$4 #absolute path needed
+MINIMUM_ABUNDANCE=${5:-"0"} #optional, number from 0-100,(default is 0) which gets inputted to devider 
+GENE_NAME=${6:-$(basename "${OUTPUT_DIR}")} #optional, use if you want intermediate files like alignments to be named something different than your output directory
+#use for one gene at a time
+
+#example: bash sylvies_devider_pipeline /home/user/upper_level_dir my_amplicon_folder reference_files/chr9.fasta /home/user/output Tc3618
+LOG_FILE="${OUTPUT_DIR}/log.txt"
+basename "${OUTPUT_DIR}"
+#other settings
+THREADS="8" #used for clair3 and devider
+MODEL_NAME="r1041_e82_400bps_hac_v500"
+MODEL_DIR="/home/tobias-lab/models"
+
+
+
+SCRIPT_NAME=$0
+script_path=$(dirname "$0")
+#make output_dir
+echo "Creating output directory"
+mkdir ${OUTPUT_DIR}
+echo "Starting pipeline for gene: ${GENE_NAME}" | tee ${LOG_FILE}  # This ensures you log the start of the pipeline
+exec > >(tee -a ${LOG_FILE}) 2>&1  # Redirect both stdout and stderr to the log file
+
+echo $@>>${OUTPUT_DIR}/log.txt #write all arguments to the log
+echo "Model used: "${MODEL_NAME} >>${OUTPUT_DIR}/log.txt
+
+
+#start making alignments
+echo "Starting to constuct alignments with minimap"
+mkdir ${OUTPUT_DIR}/alignments
+cd ${OUTPUT_DIR}/alignments
+#make initial alignment
+minimap2 -ax map-ont\
+ ${INPUT_DIR}/${REFERENCE_FASTA}\
+ ${INPUT_DIR}/${READS}\
+ > ${GENE_NAME}_alignment.sam
+#convert to bam file
+samtools view -bS\
+ ${GENE_NAME}_alignment.sam\
+ > ${GENE_NAME}_alignment.bam
+
+samtools sort\
+ -o ${GENE_NAME}_alignment_sorted.bam\
+ ${GENE_NAME}_alignment.bam
+
+samtools index\
+ -o ${GENE_NAME}_alignment_sorted.bam.bai\
+ ${GENE_NAME}_alignment_sorted.bam
+
+echo "Finished making alignments"
+
+#start making vcf with clair3
+
+echo -e "\nStarting vcf production with clair3"
+echo "alignment: ${OUTPUT_DIR}/alignments/${GENE_NAME}_alignment_sorted.bam"
+echo "reference: ${INPUT_DIR}/${REFERENCE_FASTA}"
+mkdir ${OUTPUT_DIR}/clair3_output
+docker run \
+-v ${MODEL_DIR}:/opt/models \
+-v ${INPUT_DIR}:${INPUT_DIR} \
+-v ${OUTPUT_DIR}:${OUTPUT_DIR} \
+hkubal/clair3:latest /opt/bin/run_clair3.sh \
+--bam_fn=${OUTPUT_DIR}/alignments/${GENE_NAME}_alignment_sorted.bam \
+--ref_fn=${INPUT_DIR}/${REFERENCE_FASTA} \
+--threads=${THREADS} \
+--platform="ont" \
+--model_path="/opt/models/${MODEL_NAME}" \
+--output=${OUTPUT_DIR}/clair3_output \
+--include_all_ctgs \
+--chunk_size=5000 \
+--enable_variant_calling_at_sequence_head_and_tail \
+--enable_phasing \
+--var_pct_full=1.0 \
+--ref_pct_full=1.0
+echo "Finished clair3"
+
+#devider
+
+
+
+${script_path}/intermediate/devider.sh \
+${OUTPUT_DIR}/clair3_output/merge_output.vcf.gz \
+${INPUT_DIR}/${REFERENCE_FASTA} \
+${OUTPUT_DIR}/alignments/${GENE_NAME}_alignment_sorted.bam \
+${OUTPUT_DIR}/devider_output \
+${MINIMUM_ABUNDANCE} \
+${THREADS}
+
+
+#whatshap
+mkdir ${OUTPUT_DIR}/whatshap_output
+${script_path}/intermediate/whatshap_haplotag_script.sh \
+	${OUTPUT_DIR}/whatshap_output/${GENE_NAME}_alignment_haplotagged.bam \
+	${INPUT_DIR}/${REFERENCE_FASTA} \
+	${OUTPUT_DIR}/clair3_output/phased_merge_output.vcf.gz \
+	${OUTPUT_DIR}/alignments/${GENE_NAME}_alignment_sorted.bam
